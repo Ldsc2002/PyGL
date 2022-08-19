0d95833739e35b05741ba507493828bcb77db892
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-18 21:19:58 -0600

    Replaced model

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ render.init()
 render.createWindow(1000, 1000)
 render.viewPort(0, 0, 1000, 1000)
 
-render.load('Assets/Porsche_911.obj', (1, 1, 0),  (400, 400, 400))
+render.load('Assets/Porsche.obj', (2.5, 1, 0),  (200, 200, 200))
 render.finish('Porsche')
 render.showZbuffer()
