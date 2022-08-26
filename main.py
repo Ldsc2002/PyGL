1290e8d6014777126f59aa3584a1b01ad89d5688
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-25 19:02:30 -0600

    Render porsche with rotation

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ render.viewPort(0, 0, 1000, 1000)
 t = texture('Assets/x35.bmp')
 render.load('Assets/x35.obj', (600, 300, 0),  (8, 8, 8), (0.25, 0.25, 0.25), t)
 render.finish('x35')
+
+render.clear()
+render.load('Assets/Porsche.obj', (500, 400, 0),  (100, 100, 100), (0.25, 0.25, 0.25))
+render.finish('Porsche')
