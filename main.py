d30240298afd2a2608582cd6681c0b521a48c5c2
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-30 18:00:59 -0600

    Both models in one file

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ render.viewPort(0, 0, 1000, 1000)
 render.lookAt((1, 1, 1), (0, 0, 0), (0, 1, 0))
 
 t = texture('Assets/x35.bmp')
-render.load('Assets/x35.obj', (600, 300, 0),  (8, 8, 8), (0.25, 0.25, 0.25), t)
-render.finish('x35')
-
-# render.clear()
-# render.load('Assets/Porsche.obj', (500, 400, 0),  (100, 100, 100), (0.25, 0.25, 0.25))
-# render.finish('Porsche')
+render.load('Assets/x35.obj', (300, 400, 0),  (6, 6, 6), (0.25, 0.25, 0.25), t)
+render.load('Assets/Porsche.obj', (700, 300, 0),  (50, 50, 50), (0.25, 0.25, 0.25))
+render.finish('models')
