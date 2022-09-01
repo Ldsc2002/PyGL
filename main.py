464f7da7a65350a7f00d22256eb03efc11c25701
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-31 21:12:54 -0600

    Added texture

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ render.viewPort(0, 0, 1000, 1000)
 render.lookAt((1, 0, 5), (0, 0, 0), (0, 1, 0))
 
 t = texture('Assets/face.bmp')
-render.load('Assets/face.obj', (0, 0, 0), (1, 1, 1), (0, 0, 0))
+render.load('Assets/face.obj', (0, 0, 0), (1, 1, 1), (0, 0, 0), t)
 render.finish('models')
