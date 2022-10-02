ccc66381542ad5f0d4b3c7b64edf0e6efeca7989
Ldsc2002 <luissantoscuellar@gmail.com>
2022-10-02 17:01:30 -0600

    WIP scene with multiple models

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ render.createWindow(1000, 1000)
 render.viewPort(0, 0, 1000, 1000)
 render.lookAt(V3(0 ,0 , 50), V3(0, 0, 0), V3(0, 1, 0))
 
-render.randomPoints(2500)
-render.load('Assets/sphere.obj',(0, 0, 0), (1, 1, 1), (0, 0, 0), None, "mars")
 render.load('Assets/sphere.obj',(0.7, 0.7, 0), (0.25, 0.25, 0.25), (0, 0, 0), None, "moon")
+render.load('Assets/x35.obj', (0, -0.5, 0), (0.01, 0.01, 0.01), (0, pi/2, 0), texture('Assets/x35.bmp'))
 
-render.finish('mars')
+
+render.finish('scene')
